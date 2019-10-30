fc118226cc9c59f61bf619ef968130dfdd3651ea
Carlos Kruger <carloskr@yahoo.com>
2019-10-30 16:18:12 -0400

    models and relationships

---
+++ src/main/java/com/example/springspotifyclone/models/User.java
@@ -0,0 +1,64 @@
+package com.example.springspotifyclone.models;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    @Column
+    private String password;
+
+
+
+    public User() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+@ManyToMany(fetch = FetchType.LAZY,
+cascade = {CascadeType.DETACH,
+CascadeType.MERGE, CascadeType.REFRESH})
+    @JoinTable(name="user_song",
+    joinColumns = {@JoinColumn(name = "user_id")},
+    inverseJoinColumns = @JoinColumn(name = "song_id"))
+  private List<Song> songs;
+
+    public List<Song> getSongs() {
+        return songs;
+    }
+
+    public void setSongs(List<Song> songs) {
+        this.songs = songs;
+    }
+}
